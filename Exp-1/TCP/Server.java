85895398027709689c4b48503d4656fe97955579
MUKIL S <124667264+mukil-subramaniam@users.noreply.github.com>
2024-09-27 09:42:51 +0530

    third exp

--- Exp-1/TCP/Server.java
+++ Exp-1/TCP/Server.java
@@ -3,7 +3,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 public class Server {
 public static void main(String[] args) throws IOException{
     ServerSocket serverSocket=new ServerSocket(1000);
@@ -11,7 +10,7 @@ public static void main(String[] args) throws IOException{
     DataOutputStream dataOutputStream= new DataOutputStream(socket.getOutputStream());
     dataOutputStream.writeUTF("Hii");
     dataOutputStream.close();
+
 }
-    
 }
 	
